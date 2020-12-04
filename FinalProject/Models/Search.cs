2fd80af7eb7d21aa4cfad0cf22e391bb55cbe12c
Bart Martinon <barthelemy.martinon@gmail.com>
2020-12-04 10:47:54 -0500

    Initial commit for branch Bart, Model generated from JSON

---
+++ FinalProject/Models/Search.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalProject.Models
+{
+
+    public class SearchRoot
+    {
+        public string batchcomplete { get; set; }
+        public Continue _continue { get; set; }
+        public Query query { get; set; }
+    }
+
+    public class Continue
+    {
+        public int sroffset { get; set; }
+        public string _continue { get; set; }
+    }
+
+    public class Query
+    {
+        public Searchinfo searchinfo { get; set; }
+        public Search[] search { get; set; }
+    }
+
+    public class Searchinfo
+    {
+        public int totalhits { get; set; }
+    }
+
+    public class Search
+    {
+        public int ns { get; set; }
+        public string title { get; set; }
+        public int pageid { get; set; }
+        public int size { get; set; }
+        public int wordcount { get; set; }
+        public string snippet { get; set; }
+        public DateTime timestamp { get; set; }
+    }
+}
