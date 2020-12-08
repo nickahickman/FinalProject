5db39b3f14e5d9efb2a80456bd6e5139df274640
nickahickman <nickahickman@gmail.com>
2020-12-07 22:30:41 -0500

    added AngleSharp to project and set it up to scrape wiki pages

---
+++ FinalProject/Models/WikiTextParser.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using AngleSharp;
+using System.Text.RegularExpressions;
+
+namespace FinalProject.Models
+{
+    public class WikiTextParser
+    {
+        public List<string> Sections;
+        public List<string> Paragraphs;
+
+        IConfiguration config;
+        IBrowsingContext context;
+
+        WebClient client = new WebClient();
+
+        public WikiTextParser()
+        {
+            config = Configuration.Default.WithDefaultLoader();
+            context = BrowsingContext.New(config);
+
+            Sections = new List<string>();
+            Paragraphs = new List<string>();
+        }
+
+        public async Task<List<string>> GetParagraphs(string url)
+        {
+            string source = client.DownloadString(url);
+            var HTML = await context.OpenAsync(req => req.Content(source));
+            var paragraphs = HTML.GetElementsByTagName("p").ToList();
+            List<string> paragraphList = paragraphs.Select(item => item.TextContent).ToList();
+
+            return RemoveCitations(paragraphList);
+        }
+
+        public List<string> RemoveCitations(List<string> paragraphs)
+        {
+            for (int i = 0; i < paragraphs.Count; i++)
+            {
+                paragraphs[i] = RemoveCitations(paragraphs[i]);
+            }
+
+            return paragraphs;
+        }
+
+        public string RemoveCitations(string s)
+        {
+            return Regex.Replace(s, @"\[.*?\]", "");
+        }
+    } 
+}
