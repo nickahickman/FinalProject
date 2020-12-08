056ba51fc3a15a7385fbffea347e950e0bf9b3de
L. Jensen <02Jensenl@github.com>
2020-12-08 12:06:51 -0500

    database azure connections

---
+++ FinalProject/Controllers/FavoritesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalProject.Controllers
+{
+    [Authorize]
+    public class FavoritesController : Controller
+    {
+      
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
