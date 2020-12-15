3339753fc4c13159e0f1b66c336c534ae29fbc8c
nickahickman <nickahickman@gmail.com>
2020-12-15 16:31:54 -0500

    added some styling

--- FinalProject/wwwroot/js/site.js
+++ FinalProject/wwwroot/js/site.js
@@ -2,3 +2,8 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+let audioFile = document.getElementById('player');
+
+console.log('Current State');
+console.log(`Network State: ${audioFile.networkState}`);
+console.log(`Buffered: ${audioFile.duration}`);
